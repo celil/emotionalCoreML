49fbc025dd7c514729a71ca46e85dbb1f7de2aa1
umitbozkurt <bozkurtu16@itu.edu.tr>
2018-01-25 01:50:03 +0300

    *analyze functions added *request added. *example image added.

--- EmotionalCoreML/Mobile/HomeVC/HomeVC.swift
+++ EmotionalCoreML/Mobile/HomeVC/HomeVC.swift
@@ -7,10 +7,51 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class HomeVC: UIViewController {
-
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    let model = CNNEmotions()
+    var request : VNCoreMLRequest?
+    var image : UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        image = #imageLiteral(resourceName: "celil.jpeg")
+        
+        guard let visionModel = try? VNCoreMLModel(for: model.model) else {
+            fatalError("error")
+        }
+        
+        self.request = VNCoreMLRequest(model: visionModel) { [weak self] request, error in
+            guard let result = request.results as? [VNClassificationObservation],
+                let topResult = result.first else {
+                    fatalError("celil")
+            }
+            
+            self?.resultLabel.text = "\(Int(topResult.confidence * 100)) "
+        }
+    }
+    
+}
+
+//MARK: Buttons
+extension HomeVC {
+    @IBAction func analyzeButtonClicked(_ sender: UIButton) {
+        analyze(image: image)
+    }
+}
+
+//MARK: analyze methods
+extension HomeVC {
+    fileprivate func analyze(image : UIImage) {
+        guard let request = self.request else {
+            return
+        }
+        let handler = VNImageRequestHandler(cgImage: image.cgImage!)
+        try? handler.perform([request])
+        
     }
 }
