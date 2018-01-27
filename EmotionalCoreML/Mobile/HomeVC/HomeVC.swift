8771405806b3eb622e871434afa5fb8b7a27b337
Celil Bozkurt <hi@celil.me>
2018-01-28 02:42:48 +0300

    code refactored.

--- EmotionalCoreML/Mobile/HomeVC/HomeVC.swift
+++ EmotionalCoreML/Mobile/HomeVC/HomeVC.swift
@@ -16,29 +16,15 @@ class HomeVC: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     let model = MobileNet()
-    var request : VNCoreMLRequest?
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePickerSetup()
-        
-        guard let visionModel = try? VNCoreMLModel(for: model.model) else {
-            fatalError("error")
-        }
-        
-        self.request = VNCoreMLRequest(model: visionModel) { [weak self] request, error in
-            guard let result = request.results as? [VNClassificationObservation],
-                let topResult = result.first else {
-                    fatalError("celil")
-            }
-            
-            self?.resultLabel.text = "\(Int(topResult.confidence * 100)) + \(topResult.identifier.description) "
-            
-        }
     }
 }
 
+//MARK: Setups & Configurations
 extension HomeVC {
     fileprivate func imagePickerSetup() {
         imagePicker.delegate = self
@@ -54,25 +40,14 @@ extension HomeVC {
     }
 }
 
-//MARK: analyze methods
+//MARK: Analyze methods
 extension HomeVC {
-    fileprivate func analyze(image : UIImage) {
-        guard let request = self.request else {
-            return
-        }
-        let handler = VNImageRequestHandler(cgImage: image.cgImage!)
-        try? handler.perform([request])
-        
-    }
-    
-    fileprivate func ml(image: CVPixelBuffer){
+    fileprivate func defineImage(image: CVPixelBuffer){
         guard let prediction = try? model.prediction(data: image) else {
             return
         }
-       
-        print(prediction.classLabel)
-        let value = prediction.prob.max{a, b in a.value < b.value}!.value * 100
         
+        let value = prediction.prob.max{a, b in a.value < b.value}!.value * 100
         resultLabel.text = "\(prediction.classLabel) \(String(format: "%.2f", value))%"
     }
 }
@@ -82,41 +57,9 @@ extension HomeVC: UIImagePickerControllerDelegate, UINavigationControllerDelegat
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             cameraImage.image = pickedImage
-            buffer(image: pickedImage)
+            let bufferImage = pickedImage.pixelBuffer(width: 224, height: 224)
+            defineImage(image: bufferImage!)
         }
         picker.dismiss(animated: true, completion: nil)
-    }
-}
-
-//MARK: Helpers
-extension HomeVC {
-    fileprivate func buffer(image: UIImage) {
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: 224, height: 224), true, 2.0)
-        image.draw(in: CGRect(x: 0, y: 0, width: 224, height: 224))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-        var pixelBuffer : CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        guard (status == kCVReturnSuccess) else {
-            return
-        }
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-        
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
-        
-        context?.translateBy(x: 0, y: newImage.size.height)
-        context?.scaleBy(x: 1.0, y: -1.0)
-        
-        UIGraphicsPushContext(context!)
-        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
-        UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        
-        ml(image: pixelBuffer!)
     }
 }
